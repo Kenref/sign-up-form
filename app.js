e479d4427b5ee87cc991e41f50235c8be4e5bcb5
Kenref <107287396+Kenref@users.noreply.github.com>
2023-05-02 16:04:13 +1000

    Completed project

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm-password");
-const button = document.getElementById("form")
-
-
-
+const subButton = document.getElementById("submit-button")
+const giveUp = document.getElementById("give-up")
+const hint = document.getElementById("hint")
+const vault = document.getElementById("vault")
+const body = document.querySelector("body")
 
 
 
 function checkPassword() {
-    if (password === confirmPassword) {
-        alert("Passwords match!")
-        console.log(password)
-    } else {
-        alert("passwords do not match!")
-        console.log(confirmPassword)
+    if (password.value !== confirmPassword.value) {
+        alert("Passwords do not match!")
+        password.style.border = "1px solid red"
+        confirmPassword.style.border = "1px solid red"
     }
+    else {
+        if (vault.value == 68) {
+            body.style.backgroundImage = "url(money.jpg)"
+            body.innerHTML = ""
+            body.style.backgroundSize = "cover"
+        } else {
+            alert("WRONG!")
+        }
+    }
+}
+
+function reveal() {
+    hint.style.color = "black";
 }
 
 
 
 
-button.addEventListener("click", checkPassword())
+subButton.addEventListener("click", checkPassword)
+giveUp.addEventListener("click", reveal)
